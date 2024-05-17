ad844c7e245124b4b7b90f4af21eeadea495f1c0
onenx <wzlyssn@gmail.com>
2024-05-17 07:09:00 +0000

    update autosing.py.
    Signed-off-by: onenx <wzlyssn@gmail.com>

--- autosing.py
+++ autosing.py
@@ -14,21 +14,25 @@ from sendNotify import send
 def start(cookie, username):
     try:
         s = requests.session()
-        # 永久地址已可正常访问
-        temp_addr = "https://www.wnflb2023.com/"
-        if s.get(temp_addr).status_code == 200:
-            flb_url = "www.wnflb2023.com"
-        else:
-        # 使用永久地址获取到的论坛地址
-            flb_url = get_addr()
-        headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-                   'Accept - Encoding': 'gzip, deflate',
+
+        all_url = ['www.wnflb2023.com','www.wnflb00.com','www.wnflb99.com']        
+        for i in all_url:
+            temp_addr = "https://" + i
+            if s.get(temp_addr).status_code == 200:
+                flb_url = i
+                break
+            else:
+                send("签到结果", "网站无法访问")
+                return 0
+                
+        headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                   'Accept-Encoding': 'gzip, deflate, br, zstd',
                    'Accept-Language': 'zh-CN,zh;q=0.9',
                    'cache-control': 'max-age=0',
                    'Upgrade-Insecure-Requests': '1',
                    'Host': flb_url,
                    'Cookie': cookie,
-                   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36 Edg/97.0.1072.62'}
+                   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'}
 
         # 访问Pc主页
         print(flb_url)
@@ -60,14 +64,6 @@ def start(cookie, username):
     except Exception as e:
         print("签到失败，失败原因:"+str(e))
         send("签到结果", str(e))
-
-
-def get_addr():
-    pub_page = "http://fuliba2023-1256179406.file.myqcloud.com/"
-    ret = requests.get(pub_page)
-    ret.encoding = "utf-8"
-    bbs_addr = re.findall(r'<a href=.*?><i>https://(.*?)</i></a>', ret.text)[1]
-    return bbs_addr
 
 
 if __name__ == '__main__':
