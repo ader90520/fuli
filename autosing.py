24fd3d47d786c6e0520f6b8accb4dcdabdbfe338
ader90520 <80502911+ader90520@users.noreply.github.com>
2025-01-09 10:35:03 +0800

    Update autosing.py

--- autosing.py
+++ autosing.py
@@ -21,6 +21,9 @@ def start(cookie, username):
             if s.get(temp_addr).status_code == 200:
                 flb_url = i
                 break
+                else:
+                send("签到结果", "网站无法访问")
+                return 0
                 
         headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
                    'Accept-Encoding': 'gzip, deflate, br, zstd',
